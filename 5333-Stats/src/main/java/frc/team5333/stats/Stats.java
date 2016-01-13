6803c599c3d44d29c02c3d487fa4efe609c16089
Jaci Brunning <jaci.brunning@gmail.com>
2016-01-13 23:08:46 +0800

    Submodules

---
+++ 5333-Stats/src/main/java/frc/team5333/stats/Stats.java
@@ -0,0 +1,23 @@
+package frc.team5333.stats;
+
+import jaci.openrio.toast.core.loader.annotation.Priority;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class Stats extends IterativeModule {
+
+    @Override
+    public String getModuleName() {
+        return "5333-Stats";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.1.0";
+    }
+
+    @Override
+    @Priority(level = Priority.Level.LOWEST)
+    public void prestart() {
+
+    }
+}
---
+++ 5333-WebUI/src/main/java/frc/team5333/webui/WebUI.java
@@ -0,0 +1,24 @@
+package frc.team5333.webui;
+
+import jaci.openrio.toast.lib.module.ToastModule;
+
+public class WebUI extends ToastModule {
+
+    @Override
+    public String getModuleName() {
+        return "5333-WebUI";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.1.0";
+    }
+
+    @Override
+    public void prestart() {
+
+    }
+
+    @Override
+    public void start() {}
+}
