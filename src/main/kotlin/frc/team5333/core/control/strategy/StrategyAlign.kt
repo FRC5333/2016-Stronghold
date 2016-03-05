cf0bb2b6697afc8d0fbcc693f6335038d17d1ab4
Jaci R <jaci.brunning@gmail.com>
2016-03-05 16:56:31 +0800

    Alignment is now PID Controlled

--- src/main/kotlin/frc/team5333/core/control/strategy/StrategyAlign.kt
+++ src/main/kotlin/frc/team5333/core/control/strategy/StrategyAlign.kt
@@ -2,24 +2,23 @@ package frc.team5333.core.control.strategy
 
 import frc.team5333.core.Core
 import frc.team5333.core.control.ControlLease
+import frc.team5333.core.control.InternalPID
 import frc.team5333.core.systems.DriveSystem
 import frc.team5333.core.systems.Systems
 import frc.team5333.core.vision.VisionNetwork
 
 
 class StrategyAlign : Strategy() {
-
     override fun getName(): String = "Align"
 
     var kp = 0.0
     var ki = 0.0
     var kd = 0.0
 
-    var lastError = 0.0
-    var cumulativeError = 0.0
-    var lastTime = 0L
     var started = false
+    var start_time = 0L
 
+    lateinit var internal_pid: InternalPID
     lateinit var lease: ControlLease.Lease<DriveSystem>
 
     override fun onEnable() {
@@ -27,35 +26,32 @@ class StrategyAlign : Strategy() {
         kp = Core.config.getDouble("vision.align.p", 0.7)
         ki = Core.config.getDouble("vision.align.i", 0.001)
         kd = Core.config.getDouble("vision.align.d", 0.05)
-        lastTime = System.currentTimeMillis()
         lease = Systems.drive.LEASE.acquire(ControlLease.Priority.HIGH)
+        internal_pid = InternalPID(kp, ki, kd)
+        internal_pid.target = 0.0
     }
 
-    override fun tick() {
+    override fun tick() { }
+
+    override fun tickFast() {
+        if (!started) start_time = System.currentTimeMillis()
         started = true
         if (VisionNetwork.INSTANCE.active != null) {
-            var now = System.currentTimeMillis()
-            var dt = now - lastTime
-
             var error = VisionNetwork.INSTANCE.active.offsetCenterX()
-            cumulativeError += (error * dt)
-            var deriv = (error - lastError) / dt.toDouble()
-
-            var turnV = (kp * error + kd * deriv)
+            var turnV = internal_pid.update(error)
 
             lease.use {
                 it.drive(turnV, -turnV)
             }
-
-            lastError = error
-            lastTime = now
         } else {
-            lastError = 0.0
+            internal_pid.last_error = 0.0
             Core.logger.info("Could not align: No Frame!")
         }
     }
 
     override fun isOperatorControl(): Boolean = false
 
-    override fun isComplete(): Boolean = started && (Math.abs(lastError) <= 0.05 || System.currentTimeMillis() - lastTime > 2000)   // 2 sec align time
+    override fun isFast(): Boolean = true
+
+    override fun isComplete(): Boolean = started && (Math.abs(internal_pid.last_error) <= 0.05 || System.currentTimeMillis() - start_time > 2000)   // 2 sec align time
 }
